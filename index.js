abc5a825ef05f39e670f8abaeb77cef78369c77a
Harish <33366456+HarishTeens@users.noreply.github.com>
2020-09-15 20:10:57 +0000

    try with env Vars

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ const getGifs=async ()=>{
   })
 }
 
-const makeComment=async (github_token,url,owner,repo,number,body)=>{
+const makeComment=async (github_token,url,number,body)=>{
   const octokit=new Octokit({auth:github_token})
-  const new_comment = await octokit.request('POST '+url, {
-    owner: 'octocat',
-    repo: 'hello-world',
-    issue_number: 42,
+  const new_comment = await octokit.request('POST '+url, {    
+    issue_number: number,
     body: body
   })
 }
@@ -32,17 +30,17 @@ const makeComment=async (github_token,url,owner,repo,number,body)=>{
 try {
   
   const payload = github.context.payload
-  // const github_token = core.getInput('GITHUB_TOKEN');
-  const github_token="f5831f1c9f074f3dc787f717abeda4977c6399f6";
+  const github_token = core.getInput('GITHUB_TOKEN');
+
   const context = github.context;
-  // const senderLogin=payload.sender.login;
-  const senderLogin="hari";
-  // if (context.payload.pull_request == null) {
-  //     core.setFailed('No pull request found.');
-  //     return;
-  // }
-  // const pull_request_number = context.payload.pull_request.number;
-  const pull_request_number="1";
+  const senderLogin=payload.sender.login;
+  // const senderLogin="hari";
+  if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+  }
+  const pull_request_number = context.payload.pull_request.number;
+  // const pull_request_number="1";
  
   
   
@@ -51,7 +49,7 @@ try {
     const message= '![image]('+gifURL+') \n  Hello '+senderLogin+' , '+
     'Thanks for your Commits, keep it rolling and be patient until a Reviewer merges it. '+ 
     'Until then hope this doggy keeps you company 🚀  . ';
-    makeComment(github_token,"/repos/HarishTeens/woofy/issues/1/comments","HarishTeens","woofy",pull_request_number,message);
+    makeComment(github_token,"/repos/HarishTeens/woofy/issues/1/comments",pull_request_number,message);
   });
   
 } catch (error) {
