50ab9f64ea32e49f9384389c674a9a47bfe1721e
Harish <33366456+HarishTeens@users.noreply.github.com>
2020-09-15 20:47:51 +0000

    add console

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const makeComment=async (github_token,url,number,body)=>{
     issue_number: number,
     body: body
   })
+  console.log(new_comment);
 }
 
 try {
@@ -49,7 +50,6 @@ try {
     const message= '![image]('+gifURL+') \n  Hello '+senderLogin+' , '+
     'Thanks for your Commits, keep it rolling and be patient until a Reviewer merges it. '+ 
     'Until then hope this doggy keeps you company 🚀  . ';
-    console.log(github_token,url,pull_request_number,message);
     makeComment(github_token,url,pull_request_number,message);
   });
   
