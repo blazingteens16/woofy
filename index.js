9958452c0e76575309a52fe11007fd0933e849ca
Harish <33366456+HarishTeens@users.noreply.github.com>
2020-09-15 19:33:32 +0000

    make API call from JS

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const giphy=require('@giphy/js-fetch-api');
 global.fetch = require("node-fetch");
+const {Octokit}=require('@octokit/core');
+
 
 
 const gf = new giphy.GiphyFetch('sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh')
@@ -19,13 +21,26 @@ const getGifs=async ()=>{
 try {
   
   const payload = github.context.payload
-  core.setOutput("name", payload.sender.login);
-  getGifs().then(()=>{
-    core.setOutput("gif", gifURL);
-    console.log(gifURL);  
-  });
-  getGifs();
+  const github_token = core.getInput('GITHUB_TOKEN');
+  const context = github.context;
+  if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+  }
+  const pull_request_number = context.payload.pull_request.number;
+
+  const octokit = new github.GitHub(github_token);
   
+  getGifs().then(()=>{
+    const message= '![image]('+gifURL+') \n  Hello '+payload.sender.login+' , '+
+    'Thanks for your Commits, keep it rolling and be patient until a Reviewer merges it. '+ 
+    'Until then hope this doggy keeps you company 🚀  . ';
+    const new_comment = octokit.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: message
+    });
+  });
   
 } catch (error) {
   core.setFailed(error.message);
